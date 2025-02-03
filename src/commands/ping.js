1e81f3baacb1ef0c5aefa70b2e90b7c9709c4330
Pilot_Kun <keerthi.modi1910@gmail.com>
2025-02-03 19:35:08 +0530

    Initial Commit

---
+++ src/commands/ping.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Replies with Pong!'),
+  async execute(interaction) {
+    const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+    const latency = sent.createdTimestamp - interaction.createdTimestamp;
+    const apiLatency = Math.round(interaction.client.ws.ping);
+    await interaction.editReply(`Pong! Latency is ${latency}ms. API Latency is ${apiLatency}ms.`);
+  },
+};
